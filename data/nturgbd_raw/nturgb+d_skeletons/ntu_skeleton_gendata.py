269e02c19c6f9c6f04fa9c93091b77544946526b
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2022-10-28 09:19:25 +0100

    Update ntu_skeleton_gendata.py

--- data/nturgbd_raw/nturgb+d_skeletons/ntu_skeleton_gendata.py
+++ data/nturgbd_raw/nturgb+d_skeletons/ntu_skeleton_gendata.py
@@ -142,10 +142,12 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
 
 
 if __name__ == '__main__':
+  
     parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')
     parser.add_argument('--data_path', default='../data/nturgbd_raw/nturgb+d_skeletons/')
     parser.add_argument('--ignored_sample_path',
                         default='../data/nturgbd_raw/samples_with_missing_skeletons.txt')
+      # arg.out_folder=data/nturgbd_raw/nturgb+d_skeletons/generated_skeleton_ntu/
     parser.add_argument('--out_folder', default='../data/ntu/')
 
     benchmark = ['xsub', 'xview']
