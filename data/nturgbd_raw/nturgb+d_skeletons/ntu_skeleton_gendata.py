0f9d44d4dcb413a90de00ebe870174c8b0468f48
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2022-10-28 09:26:41 +0100

    Update ntu_skeleton_gendata.py

--- data/nturgbd_raw/nturgb+d_skeletons/ntu_skeleton_gendata.py
+++ data/nturgbd_raw/nturgb+d_skeletons/ntu_skeleton_gendata.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import sys
 
 sys.path.extend(['../'])
-from data_gen.preprocess import pre_normalization
+from .preprocess import pre_normalization
 
 training_subjects = [
     1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38
