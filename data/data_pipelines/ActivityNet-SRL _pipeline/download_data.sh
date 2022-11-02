5566c039f6ad04be27fa877397de01be7e02431c
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2022-11-02 19:17:57 +0000

    Create download_data.sh

---
+++ data/data_pipelines/ActivityNet-SRL _pipeline/download_data.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Downloading script
+
+CUR_DIR=$(pwd)
+DATA_ROOT=${2:-$CUR_DIR}
+
+mkdir -p $DATA_ROOT/anet
+
+function gdrive_download () {
+	CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+	wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+	rm -rf /tmp/cookies.txt
+}
+
+function asrl_ann_dwn(){
+    echo "Downloading ActivityNet SRL annotations"
+    cd $DATA_ROOT
+    gdrive_download "1WJTRQVs-vSLmJ7I3sef3_IxE0vBtaqFX" anet_srl.zip
+    unzip anet_srl.zip && rm anet_srl.zip
+    cd $CUR_DIR
+    # The above is minimalistic download and should be fine
+    # for most cases.
+    # To get all the files:
+    # gdrive_download 1qSsD3AbWqw-KNObNg6N8xbTnF-Bg_eZn anet_verb.zip
+    # unzip anet_verb.zip && rm anet_verb.zip
+    # gdrive_download 1aZyLNP-VXS3stZpenWMuCTRF_NL2gznu anet_srl_scratch.zip
+    # unzip anet_srl_scratch.zip && rm anet_srl_scratch.zip
+    echo "Saved Folder"
+}
+
+function anet_feats_dwn(){
+    echo "Downloading ActivityNet Feats. May take some time"
+    # Courtesy of Louwei Zhou, obtained from the repository:
+    # https://github.com/facebookresearch/grounded-video-description/blob/master/tools/download_all.sh
+    cd $DATA_ROOT/anet
+    wget https://dl.fbaipublicfiles.com/ActivityNet-Entities/ActivityNet-Entities/rgb_motion_1d.tar.gz
+    tar -xvzf rgb_motion_1d.tar.gz && rm rgb_motion_1d.tar.gz
+
+    wget https://dl.fbaipublicfiles.com/ActivityNet-Entities/ActivityNet-Entities/anet_detection_vg_fc6_feat_100rois.h5
+
+    wget https://dl.fbaipublicfiles.com/ActivityNet-Entities/ActivityNet-Entities/fc6_feat_100rois.tar.gz
+    tar -xvzf fc6_feat_100rois.tar.gz && rm fc6_feat_100rois.tar.gz
+
+    gdrive_download 13tvBIEAgv4VS5dqkZBK1gvTI_Z22gRLM fc6_feat_5rois.zip
+    unzip fc6_feat_5rois.zip && rm fc6_feat_5rois.zip
+
+    gdrive_download 1a6UOK90Epz7n-dncKAeFDQP4TBgqdTS9 anet_detn_proposals_resized.zip
+    unzip anet_detn_proposals_resized.zip && rm anet_detn_proposals_resized.zip
+    cd $CUR_DIR
+}
+
+function dwn_all(){
+    asrl_ann_dwn
+    anet_feats_dwn
+}
+
+
+if [ "$1" = "asrl_anns" ]
+then
+    asrl_ann_dwn
+
+elif [ "$1" = "anet_feats" ]
+then
+    anet_feats_dwn
+elif [ "$1" = "all" ]
+then
+    dwn_all
+else
+    echo "Failed: Use download_data.sh asrl_anns | anet_feats | all"
+    exit 1
+fi
