87e62b1c90722205af2676f50a9839929c855d85
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2023-01-05 11:27:06 +0100

    Update OpenAI'sChatGPT.py

--- utils/Learning/OpenAI'sChatGPT.py
+++ utils/Learning/OpenAI'sChatGPT.py
@@ -1,1 +1,24 @@
-
+#Generative Pretraining Transformer
+#serching engine
+#influencing by what is called prompting in the background if you try to make a GPT model to produce some specific output characteritstics, 
+#it is going to do so based all on the input leading up to the point where you want to generate more of those tokens and in this case it is words.
+#This initial information "The prompt" can be deliberately crafted to illicit behaviors that you are looking for. This is your main job when you are
+#interfacing with an AI like this.It is not like these functionalities to be a search engine or any of the other things that i'm going to show you in this
+#video are prebuilt in to the model at the end of the day it is a language model attempting to behave like a generation of a chat log that is what this model
+#is and it is your job to figure out how you can structure problems in such a way that this model really wants to keep doing whatever it is that you want it to actually do.
+#Github copilot(also created by OpenAI), we have found that ChatGPT to be in many ways a far more advanced coding assistant, basically copilot codes with you
+#GhatGPT can code for you and there is a big difference there using plain language, we can just describe the program that we want 
+#this step in the develoment process was actually doable with copolit by just having maybe a comment at the top of the script that said exactly what the whole program was
+#supposed to do, but from there if we want to make modification or a change or may be an  error is happeing , you at least need to begin to know 
+#where to make that change and often where to remove code or add code, and all of that, you wouldneed to be somewhat of a programmer we are not getting hints 
+#and suggestions here.
+#GhatGPT is actually developing for us.
+#After getting these clusters created a reasonable person might want to one  just see them visualized and then two probably, realize that they really want
+#to actually see them colored according to thez category or the cluster that they belong to
+#we can just ask chatGPT to please color them, in this case intersetingly enough we wind up getting an error from the suggesqted code from ChatGPT
+#with copilot, we could probably stuck here, this would not be something we could overcome,we would probably need to either use searh engine to figure out
+#or we might need to nudge the code a bit with some of our knowledge, if we have it 
+#with GhatGPT though all we need to do is report the issue so just copy and paste the error that we see
+#from here chatGPT fixes the issues for us and it is done. we just quite literally naturally flow through the entire development process through plain language
+#ChatGPT is orders of magnitude more than Github copilot because it can engage in that developmen process with you.
+#Example: use ChatGPT: ask to write a python code to simulate Conway's Game of life, visualizing it, speed it, suggest method to use, change color, replacing giant blocks of code with other , (copy what is outputed and paste it on your editor)
